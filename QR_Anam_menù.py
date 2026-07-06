e99e3d59e5562ca4ad59103e255dbeeb0c514dd3
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2026-07-06 15:07:44 +0200

    fix

--- QR_Anam_menù.py
+++ QR_Anam_menù.py
@@ -3,7 +3,7 @@ import qrcode
 # contenuto qr.
     # Genera i nostri 5 oggetti casuali
 def generate_and_save_qr_code():
-    Menù = "https://drive.google.com/drive/u/0/folders/18wsUcFS8gyqWQNEjKol3G6ItSguhnFW9"
+    Menù = "https://drive.google.com/file/d/1tRy66famQndBxHlQVLHW7NLRoZqjPZQU/view?usp=sharing"
     print(Menù)
 
     # Genera il QR code
@@ -13,10 +13,10 @@ def generate_and_save_qr_code():
         box_size=10,
         border=4,
     )
-    qr.add_data(", ".join(Menù))
+    qr.add_data(Menù)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save(f"Menù_anam_{unique_id}.png")
+    img.save(f"Menù_anam.png")
 
 # genera e salva il nostro qr code
 generate_and_save_qr_code()
